59bdfa114489b2dc9ede79a92341b2e597829853
Tosa <cornortosa007@gmail.com>
2023-08-26 14:01:34 +0700

    Deleted SingleCountry.jsx

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import logo from "./logo.svg";
 import "./App.css";
 import CountryData from "./pages/AllCountries";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SingleCountry from "./pages/SingleCountry";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<CountryData />}></Route>
-                <Route path="/:name" element={<SingleCountry />}></Route>
             </Routes>
         </BrowserRouter>
     );
